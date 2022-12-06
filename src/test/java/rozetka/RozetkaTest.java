8be98d9d17d644d3305fe4853a95fccb57808adc
Dmytro <dim.fed.13@gmail.com>
2022-12-06 16:59:06 +0200

    Wrote BaseTest and first mock test

--- src/test/java/rozetka/RozetkaTest.java
+++ src/test/java/rozetka/RozetkaTest.java
@@ -1,6 +1,20 @@
 package rozetka;
 
+import com.rozetka.testCaseOne.TestFacade;
+import org.junit.Test;
 import rozetka.runers.BaseTest;
 
 public class RozetkaTest extends BaseTest {
+    @Test
+    public void registerNewUser() throws InterruptedException {
+        TestFacade.Interface signUp = new TestFacade().new Interface();
+        TestFacade newUser = new TestFacade.Builder()
+                .fillName("1")
+                .fillSurname("1")
+                .fillPhone("111")
+                .fillEmail("111")
+                .fillPassword("1")
+                .build();
+        signUp.successfulTest();
+    }
 }
--- src/test/java/rozetka/runers/BaseTest.java
+++ src/test/java/rozetka/runers/BaseTest.java
@@ -1,4 +1,34 @@
 package rozetka.runers;
 
+import com.rozetka.testCaseOne.HomePage;
+import com.rozetka.testCaseOne.TestPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class BaseTest {
+    protected WebDriver driver;
+    protected TestPage testPage;
+    protected HomePage homePage;
+
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        testPage = new TestPage(driver);
+        homePage = new HomePage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    }
+    @After
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
 }
